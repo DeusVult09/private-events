274b3de5401f52a61b91cea88090afab7b083c7b
Zarifa <mamadalievazara@gmail.com>
2026-05-02 00:13:39 +0500

    Set up devise to handle authentication & create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_01_180856) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_191227) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "date"
     t.string "location"
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
