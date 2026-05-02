124d00cab838a2899f24093d81e63bcdf6d552ab
Zarifa <mamadalievazara@gmail.com>
2026-05-03 01:10:05 +0500

    Write associations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  has_many :created_events, class_name: "Event", foreign_key: 'creator_id', dependent: :destroy
+  has_many :attended_events, through: :attendances, source: :event
+  has_many :attendances, foreign_key: "attendee_id", dependent: :destroy
 end
