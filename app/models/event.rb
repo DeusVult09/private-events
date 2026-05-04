842796f17e162044fb86f1a819b8128c533cccf7
Zarifa <mamadalievazara@gmail.com>
2026-05-04 21:47:26 +0500

    Add associations to a user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
-  
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :location, presence: true
+  validates :date, presence: true
+
+  belongs_to :creator, class_name: "User"
+
+  has_many :attendances, dependent: :destroy
+  has_many :attendees, through: :attendances, source: :attendees
 end
