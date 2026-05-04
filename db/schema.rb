dbbf886510af5b3df928187cbb4a509665e7921f
Zarifa <mamadalievazara@gmail.com>
2026-05-04 22:07:38 +0500

    Create and rollback migration to fix typo in Attendace join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_02_121206) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_01_192216) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "date"
-    t.text "description"
     t.string "location"
     t.string "name"
     t.datetime "updated_at", null: false
