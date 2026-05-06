8892500dc668ce143c2b58d6b5e0a2e79c067454
Zarifa <mamadalievazara@gmail.com>
2026-05-06 23:15:19 +0500

    Write create action using .build association method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Events.all
+    @events = Event.all
   end
 
   def show
@@ -12,5 +12,22 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = current_user.attended_events.build(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+  def event_params
+    params.expect(event: [:name, :location, :description, :date]) 
+  end
+
+  #def event_params
+    #params.require(:event).permit(:name, :location, :description, :date)
+  #end
+
 end
