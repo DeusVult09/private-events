17a21c42a4baa300b411e68c0de82169431e216d
Zarifa <mamadalievazara@gmail.com>
2026-05-12 00:25:56 +0500

    Write create controller action for attendance using build association method

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,11 +1,36 @@
 class AttendancesController < ApplicationController
-  def create
-    
+  before_action :set_event, only: %i[create update destroy]
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
+  def create
+    @attendance = @event.attendances.build(attendance_params)
+  
+    if @attendance.save
+      redirect_to @event, notice: "You are attending this event!"
+    else
+      render @event, alert: "Something went wrong!"
+    end
+  end
+
+
   def update
+    
   end
 
   def destroy
   end
+
+  private
+
+  def attendance_params
+    params.expect(attendance: [ :attendee, :event, :status ]) 
+  end
+
+  #def attendance_params
+    #params.require(:attendance).permit(:attendee, :event, :status)
+  #end
+
 end
