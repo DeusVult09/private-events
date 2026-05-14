c76ab4b108acd68094b30ea1fb22f5af726cb19b
Zarifa <mamadalievazara@gmail.com>
2026-05-15 02:57:09 +0500

    Preload associative data of created_events -> use .iclude, fix N+1

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,12 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_attendance
-
-  def set_attendance
-    @attendance = Attendance.find(params[:eveid])
-  end
-
 
   def show
-    @created_events = current_user.created_events
-    @pending_attendances = @attendance.event.creator == current_user && @attendance.status == "pending"
-    @attending_events = current_user.upcoming_events
+    @created_events = current_user.created_events.includes(attendances: :attendee)
+
+    @pending_attendances = Attendance.join(events)
+    .where(events: { creator_id: current_user.id })
+    .where(status: "pending")
+    
   end
 end
