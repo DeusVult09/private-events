23e1780fad713437b501a95156d6970a6694da7e
Zarifa <mamadalievazara@gmail.com>
2026-05-15 01:18:26 +0500

    Fix: Remove has_many :statuses since -> enum does that logic

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,5 @@ class Attendance < ApplicationRecord
   belongs_to :attendee, class_name: "User"
   belongs_to :event
 
-  has_many :statuses
-
   enum :status, { pending: 0, accepted: 1, declined: 2 }
 end
