1acf56f6c6ada913e2e58b9f7bc7cd3bcf4896fd
Zarifa <mamadalievazara@gmail.com>
2026-05-16 01:52:01 +0500

    Fix typo in the event model-> source: :attendee(singular)

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
   has_many :attendances, dependent: :destroy
-  has_many :attendees, through: :attendances, source: :attendees
+  has_many :attendees, through: :attendances, source: :attendee
 
   scope :upcoming, -> { where("date >= ?", Date.today) }
   scope :past, -> { where("date < ?", Date.today) }
