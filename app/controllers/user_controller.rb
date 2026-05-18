aba6d24953ba423f94d3dbe05a86926bd1be54d7
Zarifa <mamadalievazara@gmail.com>
2026-05-19 03:15:14 +0500

    Add: partials to reuse the same code for pending and declined events

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,13 +4,15 @@ class UserController < ApplicationController
   def show
     @created_events = current_user.created_events.includes(attendances: :attendee)
 
-    @pending_attendances = Attendance.joins(events)
-    .where(events: { creator_id: current_user.id })
-    .where(status: "pending")
-
     @upcoming_events = Event.joins(attendances)
     .where(attendances: { attendee_id: current_user.id, status: "accepted" })
     .upcoming
 
+    @pending_events = Event.joins(attendances)
+    .where(attendances: { attendee_id: current_user.id, status: "pending" })
+
+    @declined_events = Event.joins(attendances)
+    .where(attendances: { attendee_id: current_user.id, status: "declined" })
+    
   end
 end
