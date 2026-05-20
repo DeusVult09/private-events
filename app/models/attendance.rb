40e328e8dff9179c49e71de63a984c62d8939672
Zarifa <mamadalievazara@gmail.com>
2026-05-20 20:24:46 +0500

    Fix: Add foreign_key to the attendee association

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ApplicationRecord
-  belongs_to :attendee, class_name: "User"
+  belongs_to :attendee, class_name: "User", foreign_key: "attendee_id"
   belongs_to :event
 
   enum :status, { pending: 0, accepted: 1, declined: 2 }
