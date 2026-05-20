b742314ca9934f86061119a3f9d0c41f03018923
Zarifa <mamadalievazara@gmail.com>
2026-05-20 17:13:41 +0500

    Fix: the redirect loop + 401 -> change devise_for :users order up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "events#index"
+  devise_for :users
   resources :events do
     resources :attendances, only: [ :create, :destroy ]
   end
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [ :show ]
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
